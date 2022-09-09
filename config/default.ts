ee38647feced47bc0425823b274ec922acaa10d7
saurav <saaurav10@gmail.com>
2022-09-09 13:57:35 +0545

    created a mongo atlas db and connected

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
 export default {
   port: 1337,
-  dbUri: "mongodb://localhost:27017/vaccine_management_database",
+  // dbUri: "mongodb://localhost:27017/vaccine_management_database",
+  dbUri: "mongodb+srv://saurav:atlasuser123@vaccinedata.dy7yiy6.mongodb.net/?retryWrites=true&w=majority",
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "1y",
