f332103ed2e985dc8b2950cd155e4acab21fe94e
saurav <saaurav10@gmail.com>
2022-09-12 11:01:24 +0545

    updated the cors

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 // app.use(cors());
-app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
+app.use(cors({credentials: true, origin: 'https://vaccine-manager-frontend.herokuapp.com'}));
 
 app.use(deserializeUser);
 
